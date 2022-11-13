2695c6839f4af71749695994079db765a65fee85
Anastasiya Mironenko <nastya_911@mail.ru>
2022-11-14 00:11:44 +0300

    создана версия 1 поиск max числа

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Console.WriteLine("Введите первое число ");
+//string numberA = 
+
+int a = 12;
+int b = 3;
+
+Console.Write("a=");
+Console.Write(a + "; ");
+Console.Write("b=");
+Console.Write(b + " -> max = ");
+
+if (a<b)
+{
+        Console.Write(b);
+}
+else
+{
+        Console.Write(a);
+}
