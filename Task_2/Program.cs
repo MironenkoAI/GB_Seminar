bca15653926a824ea65ba1e9317c8925b411af19
Anastasiya Mironenko <nastya_911@mail.ru>
2022-11-14 08:50:52 +0300

    доработана программа на вывод min, добавлено условие задачи

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -1,4 +1,7 @@
-﻿Console.Write("Введите первое число: ");
+﻿// Задача 2: Напишите программу, которая принимает на вход два числа 
+//и выдаёт, какое число большее, а какое меньшее.
+
+Console.Write("Введите первое число: ");
 string? numberA = Console.ReadLine();
 int a = Convert.ToInt32(numberA);
 Console.Write("Введите второе число: ");
@@ -9,13 +12,13 @@ int b = Convert.ToInt32(numberB);
 Console.Write("a=");
 Console.Write(a + "; ");
 Console.Write("b=");
-Console.Write(b + " -> max = ");
+Console.Write(b + " -> ");
 
 if (a < b)
 {
-    Console.Write(b);
+    Console.Write("max = " + b + "; min = " + a + ".");
 }
 else
 {
-    Console.Write(a);
+    Console.Write("max = " + a + "; min = " + b);
 }
