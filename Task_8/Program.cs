a14801b6b2b4ef746e320f406258e8a82e494ad5
Anastasiya Mironenko <nastya_911@mail.ru>
2022-11-14 19:58:59 +0300

    написана программа для показа четных чисел внутри диапазона

---
+++ Task_8/Program.cs
@@ -0,0 +1,16 @@
+﻿/* Задача 8: Напишите программу, которая на вход принимает число (N), 
+а на выходе показывает все чётные числа от 1 до N.*/
+
+Console.Write("Введите целое число от 1 до N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int count = 1;
+
+while(count <= n)
+{
+    if(count % 2 == 0)
+    {
+        Console.Write(count + " ");
+    }
+    count = count + 1;
+}
