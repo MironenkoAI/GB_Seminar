e20bf254321395ffb3fdcfc5df2615f22ee733a2
Anastasiya Mironenko <nastya_911@mail.ru>
2022-11-14 12:18:32 +0300

    задача 4, нахождение max из трех чисел

---
+++ Task_4/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа 
+//и выдаёт максимальное из этих чисел.
+
+Console.Write("Введите первое число: ");
+string? numberA = Console.ReadLine();
+int a = Convert.ToInt32(numberA);
+Console.Write("Введите второе число: ");
+string? numberB = Console.ReadLine();
+int b = Convert.ToInt32(numberB);
+Console.Write("Введите третье число: ");
+string? numberC = Console.ReadLine();
+int c = Convert.ToInt32(numberC);
+
+
+Console.Write("a=");
+Console.Write(a + "; ");
+Console.Write("b=");
+Console.Write(b + "; ");
+Console.Write("c=");
+Console.Write(c + " -> max = ");
+
+
+if(a > b)
+{
+    if(a > c)
+    {
+        Console.Write(a);
+    }
+}
+
+else if(b > c)
+{
+    Console.Write(b);
+}
+
+else 
+{
+    Console.Write(c);
+}
