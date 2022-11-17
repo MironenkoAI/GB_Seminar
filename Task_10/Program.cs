027ece20e86e11ab90ac4f506dc32bf79deb538f
Anastasiya Mironenko <nastya_911@mail.ru>
2022-11-17 17:16:36 +0300

    Решение задачи показать вторую цифру трехзначного числа

---
+++ Task_10/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+//и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("Введите трехзначное число: ");
+int ThreeDigitNumber = Convert.ToInt32(Console.ReadLine());
+
+if (ThreeDigitNumber > 99 && ThreeDigitNumber < 1000)
+{
+    int DigitTwo = (ThreeDigitNumber / 10) % 10;
+    Console.WriteLine(DigitTwo);
+}
+else if (ThreeDigitNumber > -1000 && ThreeDigitNumber < -99)
+{
+    int DigitTwo = (-ThreeDigitNumber / 10) % 10;
+    Console.WriteLine(DigitTwo);
+}
+
+else
+Console.WriteLine("Вы ввели число вне диапазона! Начните сначала.");
