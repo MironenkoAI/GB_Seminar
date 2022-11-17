27bbd2a5b87192a5e5ebace19bcbafbcffa4ab0a
Anastasiya Mironenko <nastya_911@mail.ru>
2022-11-17 21:02:37 +0300

    решение задачи вывод третьей цифры числа

---
+++ Task_13/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа 
+//или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Введите число: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+
+if (Number > -1000 && Number < -99)
+{
+    Console.WriteLine(-Number % 10);
+}
+else if (Number > -100 && Number < 100)
+{
+    Console.WriteLine("В числе третьей цифры нет");
+}
+else if (Number > 99 && Number < 1000)
+{
+    Console.WriteLine(Number % 10);
+}
+while (Number > 999)
+{
+    Number = Number / 10;
+    if (Number < 1000)
+    {
+        Console.WriteLine(Number % 10);
+    }
+}
+while (Number < -999)
+{
+    Number = Number / 10;
+    if (Number > -1000)
+    {
+        Console.WriteLine(-Number % 10);
+    }
+}
