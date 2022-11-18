ba080e948bc4de2512b58cfc43c479df51a0ad48
Anastasiya Mironenko <nastya_911@mail.ru>
2022-11-19 01:50:27 +0300

    решение задачи вывести куб от 1 до N

---
+++ Task_23/Program.cs
@@ -0,0 +1,28 @@
+﻿/* Задача 23: Напишите программу, которая принимает на вход число (N) 
+и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125*/
+
+
+Console.Write("Введите любое число больше нуля: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int index = 1;
+
+while (index <= number)
+{
+    if (index == number)
+    {
+        Console.Write(index * index * index);
+    }
+    else
+    {
+        Console.Write(index * index * index + ", ");
+    }
+    index++;
+}
+
+if (number <= 0)
+{
+    Console.Write("Вы ввели число вне диапазона! Начните сначала.");
+}
