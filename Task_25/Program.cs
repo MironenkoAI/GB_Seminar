0b80b623de9dfe93caade2d89c53a5a6d9178cd0
Anastasiya Mironenko <nastya_911@mail.ru>
2022-11-24 22:25:52 +0300

    решение задачи возведение в степень

---
+++ Task_25/Program.cs
@@ -0,0 +1,22 @@
+﻿/* Задача 25: Напишите цикл, который принимает на вход 
+два числа (A и B) и возводит число A в натуральную степень B.
+Без использования функции Math.Pow.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16*/
+
+int numA = ReadInt("Введите первое число: ");
+int numB = ReadInt("Введите второе число: ");
+
+int result = numA;
+for (int count = 1; count < numB; count++)
+{
+    int temp = result * numA;
+    result = temp;
+}
+Console.WriteLine($"{numA}, {numB} -> {result}");
+
+int ReadInt(string message) 
+{     
+    Console.Write(message);     
+    return Convert.ToInt32(Console.ReadLine()); 
+}
