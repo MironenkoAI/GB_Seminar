3b47b388bec8a93b41ff3be8a17d557f72d27b12
Anastasiya Mironenko <nastya_911@mail.ru>
2022-11-24 21:32:04 +0300

    решение задачи семинара

---
+++ Task_24/Program.cs
@@ -0,0 +1,8 @@
+﻿// Задача семинара про методы и массивы
+
+int[] numbers = new int [8]; 
+for (int i = 0; i < numbers.Length; i++)
+{
+    numbers[i] = new Random().Next(0,2);
+    Console.Write(numbers[i] + " ");
+}
