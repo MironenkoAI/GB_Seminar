c04cb91c5d74bbd1138639974896be11d38461bf
Anastasiya Mironenko <nastya_911@mail.ru>
2022-11-25 17:01:29 +0300

    решение задачи сумма цифр числа с методом

---
+++ Task_27/Program.cs
@@ -0,0 +1,27 @@
+﻿/*Задача 27: Напишите программу, которая принимает на вход число 
+и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12*/
+
+Console.Write("Введите положительное число: ");
+int digit = Convert.ToInt32(Console.ReadLine());
+
+if (digit < 0)
+    Console.WriteLine("Вы ввели отрицательное число. Начните сначала.");
+
+else
+{
+    Console.WriteLine($"{digit} -> {Method(digit)}");
+
+    int Method(int digit)
+    {
+        int result = 0;
+        while (digit != 0)
+        {
+            result = result + digit % 10;
+            digit = digit / 10;
+        }
+        return result;
+    }
+}
