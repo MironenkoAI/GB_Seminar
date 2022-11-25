5e78dbf18737cd1ebb9353a2a65c69f9aa857aab
Anastasiya Mironenko <nastya_911@mail.ru>
2022-11-25 14:42:48 +0300

    решение задачи вывод массива с рандомными элементами

---
+++ Task_29/Program.cs
@@ -0,0 +1,34 @@
+﻿/*Задача 29: Напишите программу, которая задаёт массив из N элементов
+ и выводит их на экран. Вывод сделать отдельным методом.
+
+5 -> [1, 2, 5, 7, 19]
+3 -> [6, 1, 33]*/
+
+Console.Write("Задайте длину массива N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+if (N <= 0)
+{
+    Console.WriteLine("Необходимо ввести число больше нуля. Попробуйте снова.");
+}
+else
+{
+    int[] array = new int[N];
+    Method(array);
+}
+
+void Method(int[] array)
+{
+    Console.Write($"{N} -> [");
+
+    for (int i = 0; i <= N - 1; i++)
+    {
+        array[i] = new Random().Next(1, 100); //ограничила диапазон, потому что генерирует большие числа
+        if (i < N - 1)
+        {
+            Console.Write(array[i] + ", ");
+        }
+        else
+            Console.Write(array[i] + "]");
+    }
+}
